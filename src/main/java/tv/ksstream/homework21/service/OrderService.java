89c15697bc28ba6a5195d9070f2e2486899fd8a6
comradenagisych <penzaringo2010@yandex.ru>
2024-04-22 20:58:03 +0300

    Сделано ДЗ

---
+++ src/main/java/tv/ksstream/homework21/service/OrderService.java
@@ -0,0 +1,8 @@
+package tv.ksstream.homework21.service;
+
+import java.util.List;
+
+public interface OrderService {
+    List<Integer> addItems(List<Integer> items);
+    List<Integer> getItems();
+}
